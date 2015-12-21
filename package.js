02553d324062e64a36a15c2433150e3b3b432b7c
Ever <ieverx@outlook.com>
2015-12-21 03:12:54 -0800

    text

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
     name: 'ieverx:echarts',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'echarts',
+  summary: 'Use echarts with meteor.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/iEverX/meteor-echarts',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
